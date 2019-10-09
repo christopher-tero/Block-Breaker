2da9ef6c081ee84e4256f1cb268cd86bf5a95317
Christopher Tero <christopher.tero@gmail.com>
2019-10-09 15:45:59 -0500

    limited paddle movement

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    // Configuration Parameters
+    [SerializeField] float minX = 1;
+    [SerializeField] float maxX = 15;
     [SerializeField] float screenWidthInUnits = 16f;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePositionInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
