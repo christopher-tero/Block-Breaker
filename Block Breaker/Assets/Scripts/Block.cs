fda98683316bc9d437d99c3a5470e1281776d5f1
Christopher Tero <christopher.tero@gmail.com>
2019-10-16 15:27:19 -0500

    added block counter

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -6,6 +6,15 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks; // serialized for debugging purposes
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+}
