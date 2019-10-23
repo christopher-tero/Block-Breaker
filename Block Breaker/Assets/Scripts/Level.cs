c46687bc359e0452f5595d174ba69849d95d55f8
Christopher Tero <christopher.tero@gmail.com>
2019-10-23 08:57:31 -0500

    fixed problem with level changing after first block break

--- Block Breaker/Assets/Scripts/Level.cs
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -23,7 +23,7 @@ public class Level : MonoBehaviour
     public void BlockDestroyed()
     {
         breakableBlocks--;
-        if (breakableBlocks >= 0)
+        if (breakableBlocks <= 0)
         {
             sceneloader.LoadNextScene();
         }
