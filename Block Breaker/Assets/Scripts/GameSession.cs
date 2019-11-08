5c1c1ca5932c58a844724b5d0bc7a9d3c63b134a
Christopher Tero <christopher.tero@gmail.com>
2019-11-08 11:56:18 -0600

    added autoplay for testing

--- Block Breaker/Assets/Scripts/GameSession.cs
+++ Block Breaker/Assets/Scripts/GameSession.cs
@@ -9,6 +9,7 @@ public class GameSession : MonoBehaviour
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 20;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
 
     // State variables
     [SerializeField] int currentScore = 0;
@@ -47,5 +48,10 @@ public class GameSession : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
     }
 }
--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -8,18 +8,35 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minX = 1;
     [SerializeField] float maxX = 15;
     [SerializeField] float screenWidthInUnits = 16f;
+
+    // Cached References
+    GameSession theGameSession;
+    Ball theBall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePositionInUnits, minX, maxX - 1);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX - 1);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if(theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
